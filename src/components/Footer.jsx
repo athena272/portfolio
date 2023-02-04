853a89be9ad9603e0fe84a4298e746033037bea9
athena272 <guilhermera272@gmail.com>
2023-02-04 18:16:53 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,7 @@ function Footer() {
 					<div className="flex flex-wrap justify-center gap-2">
 						<a
 							href="https://www.linkedin.com/in/guigorosario/"
+							target="_blank"
 							className="bg-stone-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full">
 							<svg
 								className="w-5 h-5 fill-current"
@@ -21,7 +22,8 @@ function Footer() {
 							</svg>
 						</a>
 						<a
-							href="https://github.com/athena272"
+							href="https://github.com/athena272" 
+							target="_blank"
 							className="bg-stone-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -44,6 +46,7 @@ function Footer() {
 						</a>
 						<a
 							href="https://twitter.com/athena_272"
+							target="_blank"
 							className="bg-stone-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full">
 							<svg
 								className="w-5 h-5 fill-current"
