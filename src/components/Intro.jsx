0e9ee1a1c01cfd78fa3b0176683d3453f894f243
athena272 <guilhermera272@gmail.com>
2023-04-03 16:11:22 -0300

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,13 +6,13 @@ function Intro() {
          <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Guilherme R. Alves</h1>
          <p className="text-base md:text-xl mb-3 font-medium">Front-End Software Developer</p>
          <p className="text-sm max-w-xl mb-6 font-bold">
+            Bem-vindo ao meu portfólio! Aqui você encontrará uma seleção cuidadosamente escolhida dos meus trabalhos, habilidades e experiências profissionais.<br />
             Estudante de Engenharia da Computação altamente motivado e entusiasmado com experiência em desenvolvimento web e interesse apurado em segurança web. <br />
             "A{' '}
             <a href="https://github.com/athena272" target="_blank" className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600" rel="noreffer noopener">
                programação
             </a>
             {' '}iluminou meus dias mais sombrios." ⭐ <br />
-            Buscando aproveitar minhas habilidades e conhecimento para fazer uma contribuição significativa para a indústria de tecnologia.
          </p>
       </div>
    )
