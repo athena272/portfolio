b8cc25efd44995fb73585c85f6a8996d357c76a5
athena272 <guilhermera272@gmail.com>
2023-11-01 22:19:20 -0300

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -15,25 +15,26 @@ export default [
    },
    {
       id: 3,
-      title: 'Space-X',
-      imgUrl: '/assets/spacex.jpg',
-      stack: ['HTML', 'SASS', 'Bootstrap'],
-      link: 'https://athena272.github.io/space-x/'
-   },
-   {
-      id: 4,
       title: 'Ice Cream Parlor',
       imgUrl: '/assets/ice-cream-parlor.jpeg',
       stack: ['React', 'Javascript', 'CSS'],
       link: 'https://ice-cream-parlor-react.vercel.app/'
    },
    {
-      id: 5,
-      title: 'NodeJs API',
-      imgUrl: '/assets/nodeAPI.jpg',
-      stack: ['JavaScript', 'NodeJS'],
-      link: 'https://github.com/athena272/nodejs-api/'
+      id: 4,
+      title: 'Restaurant React',
+      imgUrl: '/assets/restaurant.jpeg',
+      stack: ['JavaScript', 'React'],
+      link: 'https://restaurant-react-iota.vercel.app/'
    },
+   {
+      id: 5,
+      title: 'Space-X',
+      imgUrl: '/assets/spacex.jpg',
+      stack: ['HTML', 'SASS', 'Bootstrap'],
+      link: 'https://athena272.github.io/space-x/'
+   },
+
    {
       id: 6,
       title: 'More Projects',
