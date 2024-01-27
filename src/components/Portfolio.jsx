4a2babd108fdc5dfb26fbd4ed07d69db96d5dfed
athena272 <guilhermera272@gmail.com>
2024-01-27 13:13:51 -0300

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,9 +6,9 @@ function Portfolio() {
    return (
       <div className="flex flex-col md:flex-row items-center justify-center">
          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {portfolio.map(project => (
+            {portfolio.map((project, index) => (
                <PortfolioItem
-                  key={project.id}
+                  key={index}
                   imgUrl={project.imgUrl}
                   title={project.title}
                   stack={project.stack}
