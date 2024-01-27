4ecf6b646d769b8009a5dfd64ec9f170476533c0
athena272 <guilhermera272@gmail.com>
2024-01-27 13:37:21 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -66,6 +66,7 @@ function App() {
 		<>
 			<button
 				name='change-theme-button'
+				aria-label='change-theme-button'
 				type="button"
 				onClick={handleThemeSwitch}
 				className="fixed p-2 z-10 right-20 top-4 bg-violet-300 dark:bg-orange-300 text-lg p-1 rounded-md"
