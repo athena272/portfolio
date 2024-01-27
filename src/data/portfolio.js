3ea077c3b31818dbf931797e1bbf572475a2a120
athena272 <guilhermera272@gmail.com>
2024-01-27 14:20:41 -0300

    update images

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,35 +2,35 @@ export default [
    {
       id: 1,
       title: 'Apple Store',
-      imgUrl: '/assets/apple-store.png',
+      imgUrl: '/assets/apple-store.webp',
       stack: ['NextJs', 'TypeScript', 'Bootstrap'],
       link: 'https://apple-store-beta.vercel.app/'
    },
    {
       id: 2,
       title: 'Healty Life Recipe',
-      imgUrl: '/assets/healty-life-recipe.png',
+      imgUrl: '/assets/healty-life-recipe.webp',
       stack: ['Angular', 'Typescript', 'Bootstrap'],
       link: 'https://healty-recipe.vercel.app/'
    },
    {
       id: 3,
       title: 'Ice Cream Parlor',
-      imgUrl: '/assets/ice-cream-parlor.jpeg',
+      imgUrl: '/assets/ice-cream-parlor.webp',
       stack: ['React', 'Javascript', 'CSS'],
       link: 'https://ice-cream-parlor-react.vercel.app/'
    },
    {
       id: 4,
       title: 'Restaurant React',
-      imgUrl: '/assets/restaurant.jpeg',
+      imgUrl: '/assets/restaurant.webp',
       stack: ['JavaScript', 'React'],
       link: 'https://restaurant-react-iota.vercel.app/'
    },
    {
       id: 5,
       title: 'Space-X',
-      imgUrl: '/assets/spacex.jpg',
+      imgUrl: '/assets/spacex.webp',
       stack: ['HTML', 'SASS', 'Bootstrap'],
       link: 'https://athena272.github.io/space-x/'
    },
@@ -38,7 +38,7 @@ export default [
    {
       id: 6,
       title: 'More Projects',
-      imgUrl: '/assets/ilusion.jpg',
+      imgUrl: '/assets/ilusion.webp',
       stack: ['Git', 'GitHub', 'Agile'],
       link: 'https://github.com/athena272?tab=repositories'
    },
