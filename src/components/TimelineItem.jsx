c8f1a2afb691a18eed0f1d0c9cd379015e1eb843
athena272 <guilhermera272@gmail.com>
2024-01-27 13:20:01 -0300

    Update TimelineItem.jsx

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -9,12 +9,12 @@ function TimelineItem({ year, title, duration, details }) {
                <span className="inline-block px-2 py-1 font-semibold text-white dark:text-stone-900 bg-stone-900 dark:bg-white rounded-md">
                   {year}
                </span>
-               <h3 className="text-lg font-semibold text-stone-900 dark:text-white">
+               <span className="text-lg font-semibold text-stone-900 dark:text-white">
                   {title}
-               </h3>
-               <div className="my-1 text-sm font-normal leading-none text-stone-400 dark:text-stone-500">
+               </span>
+               <span className="my-1 text-sm font-normal leading-none text-stone-400 dark:text-stone-500">
                   {duration}
-               </div>
+               </span>
             </p>
             <p className="my-2 text-base font-normal text-stone-500 dark:text-stone-400">
                {details}
