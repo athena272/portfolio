5093e3596a45b4c0a27b6760d0a8d3411a900cda
athena272 <guilhermera272@gmail.com>
2024-02-16 01:59:52 -0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,5 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{jsx, js}'],
 	darkMode: 'class',
-	theme: {
-		extend: {
-			fontFamily: {
-				inter: ['inter', 'serif'],
-			},
-		},
-	},
 	plugins: [],
 };
