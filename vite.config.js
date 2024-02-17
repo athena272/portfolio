aa0caa4f5aeac98671c46a2def0c54e68d056e06
athena272 <guilhermera272@gmail.com>
2024-02-17 02:22:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { minifyHtml, injectHtml } from 'vite-plugin-html'
+import { minifyHtml } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
