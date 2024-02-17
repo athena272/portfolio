fed5b28efec5f4d0378a52b839bc3da19c33b6ca
athena272 <guilhermera272@gmail.com>
2024-02-17 02:21:17 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { minifyHtml, injectHtml } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    minifyHtml(),
+    injectHtml({
+      injectData: {
+        injectScript: '<script src="content.min.js"></script>'
+      }
+    })
+  ]
 })
