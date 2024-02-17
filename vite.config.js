4b1113e77aca3c22d5fd2c93124f759f8e6ade54
athena272 <guilhermera272@gmail.com>
2024-02-17 02:32:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'terser',
-      config: {
-        include: [/content\.js$/],
-        minify: {
-          output: {
-            comments: false,
-          },
-        },
-      },
-    },
-  ]
+  plugins: [react()]
 })
