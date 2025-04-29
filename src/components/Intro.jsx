f8b045057e09a7c52f83fd8dbf2a5340f04a6b4f
athena272 <guilhermera272@gmail.com>
2025-04-29 18:35:27 -0300

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,8 +7,7 @@ function Intro({ theme }) {
          <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Guilherme R. Alves</h1>
          <p className="text-base md:text-xl mb-3 font-medium">Front-End Software Developer</p>
          <p className="text-sm max-w-xl mb-6 font-bold">
-            Bem-vindo ao meu portfólio! Aqui você encontrará uma seleção cuidadosamente escolhida dos meus trabalhos, habilidades e experiências profissionais.<br />
-            Estudante de Engenharia da Computação altamente motivado e entusiasmado com experiência em desenvolvimento web e interesse apurado em segurança web. <br />
+            Sou um Desenvolvedor de Software apaixonado, com forte atuação em tecnologias Front-End, e atualmente curso Bacharelado em Engenharia de Computação na Universidade Federal de Sergipe. Ao longo dos últimos anos, contribuí para projetos variados, desde plataformas de empregos no BNE (Banco Nacional de Empregos) até aplicativos mobile em Flutter e sites de provedor de internet, sempre focando em entregar soluções eficientes, de alto desempenho e centradas no usuário. Minhas habilidades abrangem React, Next.js, TypeScript, Node.js e Angular, além de práticas de SEO e otimização de performance. Também possuo experiência em pesquisa de segurança (web bug hunting), identificando e reportando vulnerabilidades de forma responsável. Movido por aprendizado contínuo, valorizo a colaboração, código limpo e o uso da tecnologia para criar experiências digitais de impacto<br />
             "A{' '}
             <a href="https://github.com/athena272" target="_blank" style={{ textDecoration: 'underline', color: `${theme === 'dark' ? '#38BDF8' : '#4C00FF'}` }} rel="noreffer noopener">
                programação
