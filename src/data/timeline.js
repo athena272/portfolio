46052c52c7ae7dab12775df454565820c77cc080
athena272 <guilhermera272@gmail.com>
2025-06-17 21:06:39 -0300

    Update timeline.js

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,10 +1,15 @@
 export default [
 	{
 		year: '2022/Atual',
-		title: 'Desenvolvedor Front-End Efetivado',
-		duration: '2 anos e 8 meses',
+		title: 'Desenvolvedor Front-End',
+		duration: '2 anos e 10 meses',
 		details:
-			`Atuação no desenvolvimento e manutenção das plataformas Trabalha Brasil e RedTrabaje, criando novas páginas, corrigindo bugs e otimizando a performance do site. Implementação de boas práticas de SEO e acompanhamento de métricas de performance via Google Analytics. Colaboração com equipes multidisciplinares para implantação de novas funcionalidades e melhorias contínuas. Tecnologias: HTML, CSS, JavaScript, jQuery, Bootstrap, React, Next.js, Regex, JSON, Git (Azure Repositories, Git Flow), Notion, API REST, Cypress, Gulp, Google Analytics Resultado: Reduziu o tempo médio de carregamento das páginas em 30%, gerando um aumento de 15% no engajamento dos usuários e maior volume de vagas anunciadas`
+			`Atuação no desenvolvimento e manutenção das plataformas Trabalha Brasil e RedTrabaje, criando novas páginas, corrigendo bugs e
+			otimizando a performance do site. Implementação de boas práticas de SEO e acompanhamento de métricas de performance via Google
+			Analytics. Colaboração com equipes multidisciplinares para implantação de novas funcionalidades e melhorias contínuas. Tecnologias:
+			HTML, CSS, JavaScript, jQuery, Bootstrap, React, Next.js, Regex, JSON, Git (Azure Repositories, Git Flow), Notion, API REST, Cypress,
+			Gulp, Google Analytics Resultado: Reduziu o tempo médio de carregamento das páginas em 30%, gerando um aumento de 15% no
+			engajamento dos usuários e maior volume de vagas anunciadas.`
 	},
 	{
 		year: 'Atual',
@@ -14,13 +19,6 @@ export default [
 			"Caço falhas em sites, de maneira legalizada em troca de recompensas e reputação.",
 	},
 	{
-		year: '2022',
-		title: 'Estágio Desenvolvedor Flutter',
-		duration: '7 meses',
-		details:
-			'Ajudei a dar manuntenção e criar aplicativos com a tecnologia Flutter para a empresa SoftGreen em um ambiente com uma pilha de outras linguagens e ferramentas, das quais precisei estudar e ir me capacitando.',
-	},
-	{
 		year: '2024',
 		title: 'Tecnólogo em Análise e Desenvolvimento de Sistemas',
 		duration: '1 ano',
@@ -28,11 +26,30 @@ export default [
 			'Preparação e formação em desenvolvimento de sistemas, com foco em diferentes aspectos da construção de software, incluindo análise de requisitos, modelagem de dados e design de software.',
 	},
 	{
+		year: '2022',
+		title: 'Desenvolvedor Flutter',
+		duration: '7 meses',
+		details:
+			`Responsável pela manutenção e desenvolvimento de aplicativos mobile utilizando o framework Flutter. Criação e implementação de
+			interfaces de usuário responsivas e novas funcionalidades para melhorar a experiência do usuário. Integração com APIs e serviços backend
+			para garantir comunicação eficiente entre cliente e servidor. Resultado: Aumentou a adoção de usuários em 20% após a reformulação da
+			interface e otimização de performance, resultando em maior número de usuários ativos diariamente.`,
+	},
+	{
 		year: '2021',
 		title: "Bacharel em Engenharia da Computação",
 		duration: '4.5 anos',
 		details:
 			'Estou buscando o conhecimento que um diploma de Engeharia da Computação têm a oferecer. Estou aprendendo sobre arquitetura de computadores, teoria da ciência da computação, estrutura de dados, variáveis formas de programar e construir algoritmos.',
+	},
+	{
+		year: '2021',
+		title: 'Desenvolvedor .NET C#',
+		duration: '4 meses',
+		details:
+			`Realizou manutenção e melhorias em páginas internas usando JavaScript e jQuery. Contribuiu na depuração e solução de problemas tanto
+			no frontend quanto no backend. Resultado: Automatizou tarefas repetitivas de processamento de dados, reduzindo a sobrecarga operacional
+			em 25% e acelerando atualizações em ferramentas internas.`,
 	},
 	{
 		year: '2018',
